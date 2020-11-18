fcbcb15a8d230066a2a8bc8c8b9a5587b6cb1b27
helloiris0216 <l66074106@gs.ncku.edu.tw>
2020-11-18 23:16:05 +0800

    Feat:coding, cannot switch to city_layout.xml
    Issue #003

--- app/src/main/java/com/example/kao_hw03_listview_ex/MainActivity.java
+++ app/src/main/java/com/example/kao_hw03_listview_ex/MainActivity.java
@@ -20,13 +20,15 @@ import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
+    private String TAG = "MainActivity";
     private Context context;
     private ListView listViewId;
     private String[] cityName;
-    private ListAdapter listAdapter;
-    private String TAG = "MainActivity";
     private int[] cityPic= {R.drawable.tainan, R.drawable.taipei, R.drawable.taitung,
                             R.drawable.taoyuan, R.drawable.yilan};
+
+//    private int[] cityIcon={R.drawable.icon_tainan, R.drawable.icon_taipei, R.drawable.icon_taitung,
+//                            R.drawable.icon_taoyuan, R.drawable.icon_yilan};
     private List<Map<String, Object>> itemList;
 
     @Override
@@ -37,26 +39,48 @@ public class MainActivity extends AppCompatActivity {
         context = this;
         setTitle("ListView_Ex");
 
+        //初始化
         listViewId = (ListView) findViewById(R.id.listView_id);
         setList();
-        setAdaptor();
+
     }
 
+    //將cityName & cityPic放入Map後，再放入List內
     private void setList() {
         //get data
         itemList = new ArrayList<Map<String, Object>>();
         cityName = getResources().getStringArray(R.array.city);
+        Log.d(TAG, "setList: cityName = " +cityName);
 
         //put data
         for (int i=0; i<cityName.length; i++){
-            HashMap<String, Object> data = new HashMap<String, Object>();
+            HashMap<String, Object> data = new HashMap<>();
             data.put("CITYNAME", cityName[i]);
             data.put("CITYPIC", cityPic[i]);
 
+            Log.d(TAG, "setList: CITYNAME = " +cityName);
+            Log.d(TAG, "setList: CITYPIC = " +cityPic);
+
             itemList.add(data);
         }
+        setAdaptor();
     }
 
+    //將資料(itemList)放入Adaptor內
+    //再將adaptor給listView(listViewFood)
+    private void setAdaptor() {
+        Log.d(TAG, "setAdaptor: ");
+        //SimpleAdaptor
+        SimpleAdapter adaptor = new SimpleAdapter(context, itemList, R.layout.city_layout,
+                new String[]{"CITYNAME", "CITYPIC"},
+                new int[]{R.id.textView_city, R.id.imageView_city});
+
+        listViewId.setAdapter(adaptor);
+
+        setListener();
+    }
+
+    //監聽adaptor，取出放在Map內的資料
     private void setListener() {
         listViewId.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -69,14 +93,5 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    private void setAdaptor() {
-        //SimpleAdaptor
-        SimpleAdapter adaptor = new SimpleAdapter(context, itemList, R.layout.city_layout,
-                new String[]{"CITYNAME", "CITYPIC"},
-                new int[]{R.id.textView_city, R.id.imageView_city});
 
-        listViewId.setAdapter(adaptor);
-
-        setListener();
-    }
 }
